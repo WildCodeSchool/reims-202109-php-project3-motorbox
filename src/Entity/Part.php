b4e2dd4b503424b22e6b2777920937a137e650bb
Amandine Lalle <amandinelalle51@gmail.com>
2021-12-08 15:49:08 +0100

    create entity part

---
+++ src/Entity/Part.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PartRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PartRepository::class)
+ */
+class Part
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $lifespan;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLifespan(): ?int
+    {
+        return $this->lifespan;
+    }
+
+    public function setLifespan(int $lifespan): self
+    {
+        $this->lifespan = $lifespan;
+
+        return $this;
+    }
+}
