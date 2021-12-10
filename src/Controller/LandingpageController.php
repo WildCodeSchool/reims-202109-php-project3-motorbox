532717e1a6367d60f1816d617a3f45fc4e63d086
James <jeanmariejames51@gmail.com>
2021-12-10 10:58:25 +0100

    made some change in the controller and css and landingpage view

--- src/Controller/LandingpageController.php
+++ src/Controller/LandingpageController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class LandingpageController extends AbstractController
 {
-    /**
-     * @Route("/home", name="home")
-     */
+    #[Route('/', name: 'landingpage')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('landingpage/landingpage.html.twig', [
+            'controller_name' => 'LandingpageController',
+        ]);
     }
 }
