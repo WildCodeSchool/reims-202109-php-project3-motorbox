626eb61f1e870b6060e7713e42320ca6de286468
So-Gau <sophie.gaubert10@gmail.com>
2021-12-15 15:08:41 +0100

    added content in fixtures

--- src/DataFixtures/VehicleFixtures.php
+++ src/DataFixtures/VehicleFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Vehicle;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class VehicleFixtures extends Fixture
 {
+    public const VEHICLES = [
+        [
+            'name' => 'Kitt',
+            'brand' => 'moto',
+            'model' => 'moto',
+            'productYear' => 1920,
+            'usedHour' => '10',
+        ]
+    ];
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach (self::VEHICLES as $key => $vehicleData) {
+            $vehicle = new Vehicle();
+            $vehicle->setName($vehicleData['name']);
+            $vehicle->setBrand($vehicleData['brand']);
+            $vehicle->setModel($vehicleData['model']);
+            $vehicle->setProductYear($vehicleData['productYear']);
+            $vehicle->setUsedHour($vehicleData['usedHour']);
+            $this->addReference('vehicle_' . $key, $vehicle);
 
+            $manager->persist($vehicle);
+        }
         $manager->flush();
     }
 }
