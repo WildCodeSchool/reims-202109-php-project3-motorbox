4b350325a88724088e2984bb415ca59e7328c308
Nibor51 <jonval.robin@gmail.com>
2022-01-11 11:55:39 +0100

    reorder use of VehiculeController

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Vehicle;
-use App\Form\VehicleType;
-use App\Repository\VehicleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Vehicle;
+use App\Form\VehicleType;
+use App\Repository\VehicleRepository;
 
 #[Route('/vehicle')]
 class VehicleController extends AbstractController
