b36008105284101b537ca3c709d9f69dba7cc8cc
Nibor51 <jonval.robin@gmail.com>
2022-01-11 12:14:05 +0100

    create crud part on vehicleController

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -8,7 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Vehicle;
+use App\Entity\Part;
 use App\Form\VehicleType;
+use App\Form\PartType;
 use App\Repository\VehicleRepository;
 
 #[Route('/vehicle')]
@@ -77,5 +79,88 @@ class VehicleController extends AbstractController
         }
 
         return $this->redirectToRoute('vehicle_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}/parts', name: 'vehicle_parts', methods: ['GET'])]
+    public function parts(Vehicle $vehicle): Response
+    {
+        return $this->render('vehicle/parts.html.twig', [
+            'vehicle' => $vehicle,
+        ]);
+    }
+
+    #[Route('/{id}/parts/new', name: 'vehicle_parts_new', methods: ['GET', 'POST'])]
+    public function partsNew(Request $request, Vehicle $vehicle, EntityManagerInterface $entityManager): Response
+    {
+        $part = new Part();
+        $part->setVehicle($vehicle);
+        $form = $this->createForm(PartType::class, $part);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($part);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('vehicle_parts', [
+                'id' => $vehicle->getId(),
+            ], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vehicle/parts_new.html.twig', [
+            'vehicle' => $vehicle,
+            'part' => $part,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/parts/{partId}', name: 'vehicle_parts_show', methods: ['GET'])]
+    public function partsShow(Vehicle $vehicle, Part $part): Response
+    {
+        return $this->render('vehicle/parts_show.html.twig', [
+            'vehicle' => $vehicle,
+            'part' => $part,
+        ]);
+    }
+
+    #[Route('/{id}/parts/{partId}/edit', name: 'vehicle_parts_edit', methods: ['GET', 'POST'])]
+    public function partsEdit(
+        Request $request,
+        Vehicle $vehicle,
+        Part $part,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $form = $this->createForm(PartType::class, $part);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('vehicle_parts', [
+                'id' => $vehicle->getId(),
+            ], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vehicle/parts_edit.html.twig', [
+            'vehicle' => $vehicle,
+            'part' => $part,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/parts/{partId}', name: 'vehicle_parts_delete', methods: ['POST'])]
+    public function partsDelete(
+        Request $request,
+        Vehicle $vehicle,
+        Part $part,
+        EntityManagerInterface $entityManager
+    ): Response {
+        if ($this->isCsrfTokenValid('delete' . $part->getId(), strval($request->request->get('_token')))) {
+            $entityManager->remove($part);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('vehicle_parts', [
+            'id' => $vehicle->getId(),
+        ], Response::HTTP_SEE_OTHER);
     }
 }
