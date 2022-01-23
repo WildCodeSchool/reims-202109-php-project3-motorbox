4757923a2d132822a2130e023bafe27a6577a9d5
Robin Jonval <88200918+nibor51@users.noreply.github.com>
2022-01-23 07:03:25 +0100

    fixed obsolete use statement

--- src/Entity/Part.php
+++ src/Entity/Part.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 
 use App\Repository\PartRepository;
 use Doctrine\ORM\Mapping as ORM;
-use phpDocumentor\Reflection\Types\Integer;
 
 /**
  * @ORM\Entity(repositoryClass=PartRepository::class)
