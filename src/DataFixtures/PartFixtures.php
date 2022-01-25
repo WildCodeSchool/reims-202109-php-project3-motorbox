2dc376a3ec34bc1292274d2e61e4b29c592e846d
So-Gau <sophie.gaubert10@gmail.com>
2022-01-25 14:56:43 +0100

    add part use time in partFixture

--- src/DataFixtures/PartFixtures.php
+++ src/DataFixtures/PartFixtures.php
@@ -14,26 +14,31 @@ class PartFixtures extends Fixture implements DependentFixtureInterface
             'name' => 'Battery',
             'lifespan' => 10,
             'vehicleReference' => 'vehicle_0',
+            'partUseTime' => '0.00',
         ],
         [
             'name' => 'Gas tank',
             'lifespan' => 65,
             'vehicleReference' => 'vehicle_0',
+            'partUseTime' => '0.00',
         ],
         [
             'name' => 'Front tire',
             'lifespan' => 5,
             'vehicleReference' => 'vehicle_0',
+            'partUseTime' => '0.00',
         ],
         [
             'name' => 'Handle Bar',
             'lifespan' => 23,
             'vehicleReference' => 'vehicle_0',
+            'partUseTime' => '0.00',
         ],
         [
             'name' => 'Brake pedal',
             'lifespan' => 35,
             'vehicleReference' => 'vehicle_0',
+            'partUseTime' => '0.00',
         ],
     ];
 
@@ -43,6 +48,7 @@ class PartFixtures extends Fixture implements DependentFixtureInterface
             $part = new Part();
             $part->setName($partData['name']);
             $part->setLifespan($partData['lifespan']);
+            $part->setPartUseTime($partData['partUseTime']);
             $part->setVehicle(
                 $this->getReference($partData['vehicleReference'])
             );
